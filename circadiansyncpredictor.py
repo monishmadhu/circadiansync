28272c7b003fd720552bc66d875e8488b0c0d939
monishmadhu <159864962+monishmadhu@users.noreply.github.com>
2024-02-12 23:37:50 -0600

    Update circadiansyncpredictor.py

--- circadiansyncpredictor.py
+++ circadiansyncpredictor.py
@@ -8,13 +8,20 @@ def main():
 
     # Add widgets to the sidebar
     st.sidebar.title("CircadianSync")
-    st.sidebar.image("path/to/your/image.png", use_column_width=True)
+    # st.sidebar.image("https://raw.githubusercontent.com/your_username/your_repository/main/path/to/your/image.png", use_column_width=True)
 
-    st.sidebar.write("CircadianSync is a Machine Learning model that intakes the gene expression levels of patients in order to analyze and predict whether they have pancreatic adenocarcinoma, circadian dysfunction, neither, or both.")
     st.sidebar.write("To use CircSync to predict a patient's diagnosis, upload a CSV/Excel file with their gene expression levels as numerical values in it. Make sure there are two columns: 1 for gene names labeled GeneID and 1 for the values labeled ExpressionLevels")
 
     # Add a file uploader widget to the sidebar
     uploaded_file = st.sidebar.file_uploader("Upload a file", type=["csv"])
+
+    # Add a big image to the main portion of the app
+    # st.image("https://raw.githubusercontent.com/your_username/your_repository/main/path/to/your/big_image.png", use_column_width=True)
+
+    # Add a title and text underneath the image
+    st.header("CircadianSync")
+    st.write("CircadianSync is a Machine Learning model that intakes the gene expression levels of patients in order to analyze and predict whether they have pancreatic adenocarcinoma, circadian dysfunction, neither, or both.")
+
 
     # Process the uploaded file (if any)
     if uploaded_file is not None:
