30d7e2cb99352e0ede28e9e23ce36fdfde9c6703
monishmadhu <159864962+monishmadhu@users.noreply.github.com>
2024-02-14 20:06:26 -0600

    Update circadiansyncpredictor.py

--- circadiansyncpredictor.py
+++ circadiansyncpredictor.py
@@ -1,37 +1,93 @@
 import streamlit as st
+import pandas as pd
+import os
+import joblib
 
 def main():
     with open("circsync_css.css", "r") as f:
         css = f.read()
     st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
     
-    st.title("CircadianSync")
+    st.markdown('<h1 class="centered-title">Circadian Sync</h1>', unsafe_allow_html=True)
 
     # Add content to the main area of the app
-    st.write("CircadianSync is a Machine Learning model that intakes the gene expression levels of patients in order to analyze and predict whether they have pancreatic adenocarcinoma, circadian dysfunction, neither, or both.")
+    st.markdown('<div class="main-content">CircadianSync is a Machine Learning model that intakes the gene expression levels of patients in order to analyze and predict whether they have pancreatic adenocarcinoma, circadian dysfunction, neither, or both.</div>', unsafe_allow_html=True)
 
     # Add widgets to the sidebar
     st.sidebar.title("CircSync Predictor")
-    # st.sidebar.image("https://raw.githubusercontent.com/your_username/your_repository/main/path/to/your/image.png", use_column_width=True)
+    st.sidebar.markdown('<div class="sidebar-content">To use CircSync to predict a patient\'s diagnosis, upload a CSV/Excel file with their gene expression levels as numerical values in it. Make sure there are two columns: one for gene names labeled GeneID and one for the values labeled ExpressionLevels</div>', unsafe_allow_html=True)
 
-    st.sidebar.write("To use CircSync to predict a patient's diagnosis, upload a CSV/Excel file with their gene expression levels as numerical values in it. Make sure there are two columns: one for gene names labeled GeneID and one for the values labeled ExpressionLevels")
-
-    # Add a file uploader widget to the sidebar
+    # Upload the CSV file
     uploaded_file = st.sidebar.file_uploader("Upload a file", type=["csv"])
 
+    if uploaded_file is not None:
+        # Read the CSV file into a pandas DataFrame
+        df = pd.read_csv(uploaded_file)
+    
+        # Remove the first column
+        df.drop(df.columns[0], axis=1, inplace=True)
+    
+        # Move the second column to the first position
+        cols = df.columns.tolist()
+        cols = cols[-1:] + cols[:-1]
+        df = df[cols]
+    
+        df.to_csv("modified_file.csv", index=False)
+
+        # Load the test data from a file
+        def load_test_data(file_path):
+            df = pd.read_csv(file_path)  # assuming it's a CSV file
+            return df
+
+        # Preprocess the test data
+        def preprocess_test_data(df):
+            # Convert DataFrame values to numeric
+            df = df.apply(pd.to_numeric, errors='coerce')  # coerce errors to NaN
+
+            # Remove rows containing NaN values
+            df.dropna(inplace=True)
+
+            return df
+
+        # Calculate gene expression ratios
+        def calculate_ratios(df):
+            # Calculate the sum of all cells in the file
+            file_sum = df.values.sum()
+
+            # Calculate ratios for each cell in the file
+            file_ratios = df.values / file_sum
+
+            # Flatten the nested list structure
+            flattened_ratios = file_ratios.flatten()
+
+            return flattened_ratios.reshape(1, -1)  # Reshape for prediction
+
+        # Provide the folder path containing test files
+        file_path = 'modified_file.csv'
+        
+        # Check if the file exists
+        if os.path.exists(file_path) and file_path.endswith('.csv'):
+            # Load and preprocess the test data
+            test_data = load_test_data(file_path)
+            test_data = preprocess_test_data(test_data)
+
+            # Calculate gene expression ratios for the test data
+            test_ratios = calculate_ratios(test_data)
+
+            # Load the model from the file
+            model = joblib.load('random_forest_model_ISEF (2).pkl')
+
+            # Now you can use the loaded model to make predictions
+            predictions = model.predict(test_ratios)
+
+            # Print the predicted scenario for the file
+            st.write(f"File: {file_path}, Predicted Scenario: {predictions}")
+
     # Dropdown menu in the sidebar
-    #selected_option = st.sidebar.selectbox("Select an model", ["Random Forest", "Gradient Boosting Classifier", "K Neighbors", "Decision Tree Classifier"])
+    selected_option = st.sidebar.selectbox("Select a model", ["Random Forest", "Gradient Boosting Classifier", "K Neighbors", "Decision Tree Classifier"])
 
     # Display selected option
     st.write(f"You selected: {selected_option}")
 
-    # Add a big image to the main portion of the app
-    st.image("https://github.com/monishmadhu/circadiansync/blob/main/logo.png", use_column_width=True)
-
-    # Process the uploaded file (if any)
-    if uploaded_file is not None:
-        file_details = {"FileName": uploaded_file.name, "FileType": uploaded_file.type}
-        st.write(file_details)
-  
 if __name__ == "__main__":
     main()
